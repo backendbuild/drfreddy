f7d7c5a54c1a9387ca9bd84cfa43d5a6193135f5
unknowperson <simrspolri@gmail.com>
2024-06-26 11:28:46 +0700

    first commit

--- src/landing/carouselPage.tsx
+++ src/landing/carouselPage.tsx
@@ -1,12 +1,5 @@
-"use client"
+"use client";
 import React, { useState, useEffect } from 'react';
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel"; // Adjust the import path as necessary
 import Image from 'next/image';
 
 const CarouselPage: React.FC = () => {
@@ -17,38 +10,28 @@ const CarouselPage: React.FC = () => {
     { src: '/images/puska.png', alt: 'Image 3' },
   ];
 
-  const handlePrevious = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
-  };
-
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
-  };
-
   useEffect(() => {
-    const interval = setInterval(() => {
-      handleNext();
-    }, 3000); // Change slide every 3 seconds
+    const handleNext = () => {
+      setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+    };
+
+    const interval = setInterval(handleNext, 3000); // Change slide every 3 seconds
 
     return () => clearInterval(interval); // Clean up the interval on component unmount
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentIndex]); // Only reset the interval when the currentIndex changes
+  }, [images.length]);
 
   return (
-    <div className="m-24">
-      <Carousel>
-        <CarouselContent currentIndex={currentIndex}>
-          {images.map((image, index) => (
-            <CarouselItem key={index}>
-              <div className="relative flex items-center justify-center">
-                <Image src={image.src} alt={image.alt} width={1200} height={800} objectFit="cover" />
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious onClick={handlePrevious} />
-        <CarouselNext onClick={handleNext} />
-      </Carousel>
+    <div className="relative overflow-hidden m-24">
+      <div
+        className="flex transition-transform duration-500"
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
+        {images.map((image, index) => (
+          <div key={index} className="min-w-full flex-shrink-0 flex justify-center items-center">
+            <Image src={image.src} alt={image.alt} width={1200} height={800} objectFit="cover" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
